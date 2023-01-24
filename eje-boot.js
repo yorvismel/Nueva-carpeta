afd6767f3a6997f856e677fa266c0cf226164872
yorvismel <112412149+yorvismel@users.noreply.github.com>
2023-01-24 09:00:43 -0500

    listas

---
+++ eje-boot.js
@@ -0,0 +1,15 @@
+
+
+let lista = {
+    nombre: 'Yorvis Alfredo Melendez',
+    edad: 39,
+    desarrollador: true,
+    fechaDeNacimiento: new Date('june, 28 1983'),
+    libro: {
+      titulo: 'El Principito',
+      autor: 'Antoine de Saint-Exupéry',
+      fecha: new Date('1983, 6, 28'),
+      url: 'https://www.amazon.com/Principito-Antoine-Saint-Exupery/dp/2070360022'
+    }
+  };
+console.log(lista)
